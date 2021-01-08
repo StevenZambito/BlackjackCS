aa3a55b6608d4b8d8dbbf987d7abcab5648c324f
StevenZambito <zambito.steven@icloud.com>
2021-01-08 10:57:19 -0500

    Started creating classes

--- Program.cs
+++ Program.cs
@@ -2,11 +2,68 @@
 
 namespace BlackjackCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Player
     {
-      Console.WriteLine("Welcome to C#");
+        public string Name { get; set; }
+        public int HandTotal { get; set; }
+
+        public int Hit()
+        {
+
+        }
+
+        public int Stand()
+        {
+
+        }
+
+
     }
-  }
+
+    class Card
+    {
+        public string Faces { get; set; }
+        public string Suits { get; set; }
+
+        public int Value()
+        {
+
+        }
+
+
+    }
+
+    class Deck
+    {
+        public string ListOfAllFaces { get; set; }
+
+        public string listOfAllSuits { get; set; }
+
+        public string cards { get; set; }
+
+        GenerateNewDeck()
+        {
+
+        }
+
+        ShuffleDeck()
+        {
+
+        }
+
+        DealCards()
+        {
+
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
 }
